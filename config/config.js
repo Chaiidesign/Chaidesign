3eb40c32cd0444415dbb42ff874d7ba2230362b5
Chaitanya <chai.design@cca.edu>
2025-07-16 23:23:29 -0700

    Update config.js

--- config/config.js
+++ config/config.js
@@ -25,6 +25,8 @@ const chatConfig = {
     "I feel overwhelmed trying to keep up with AI trends.",
     "I am anxious about the future.",
     "I am anxious about the future.",
+    "I am anxious about the future.",
+    "I am anxious about the future.",
   ],
   chatInputPlaceholder: "Chat with this agent...",
   maxChatHeight: 200,
